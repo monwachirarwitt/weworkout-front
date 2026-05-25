89dd214260171b7a9c09c1ae12ea5ab92763b273
monwachirarwitt <monwachirarwit.rattana@gmail.com>
2026-05-25 14:08:36 +0700

    fix: update axios interceptors to handle expired authentication tokens correctly

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // ตั้งค่าให้ยิงไปที่พอร์ต 8000 ของหลังบ้านเราเสมอ
 const axiosInstance = axios.create({
-  baseURL: 'https://weworkout-back.onrender.com/api',
+  baseURL: 'https://weworkout-back.onrender.com',
 });
 
 // แนบ Token ไปกับ Header อัตโนมัติเวลาคุยกับหลังบ้าน (ตามที่โค้ดส่วนอื่นสมมติไว้)
